4a3ee29554169e3e893c73a2d2629afaef0e0ee9
rayhan-18 <mr0031689@gmail.com>
2025-09-27 12:23:40 +0700

    update clas exchange,installment,loan

--- Models/Exchange.cs
+++ Models/Exchange.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Corvus.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Corvus.Models
+namespace BraveHeroCooperation.Models
 {
-    internal class Exchange
+    public class Exchange
     {
+        public int Id { get; set; }
+        [Required] public int MemberId { get; set; }
+        public Member Member { get; set; } = null!;
+        public string ExchangeId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal AmountExchanged { get; set; }
+        public decimal Fee { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime ExchangeDate { get; set; }
+        public decimal TotalAmountExchanged { get; set; }
     }
 }
