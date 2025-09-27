0a45adfa0b23397440b7f1b1c9ac1a15f62a889c
Codenames-Ren <renmizuki38@gmail.com>
2025-09-27 12:14:25 +0700

    Update class Configuration

--- Models/Configuration.cs
+++ Models/Configuration.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Corvus.Models
 {
-    internal class Configuration
+    public class Configuration
     {
+        public int Id { get; set; }
+        [MaxLength(4000)] public string terminologi1 { get; set; }
+        [MaxLength(4000)] public string terminologi2 { get; set; }
+        [MaxLength(4000)] public string terminolog3 { get; set; }
+
+        public decimal exchangeRate { get; set; }
+        public decimal transferAcrossFee { get; set; }
+        public decimal transferInhouseFee { get; set; }
     }
 }
