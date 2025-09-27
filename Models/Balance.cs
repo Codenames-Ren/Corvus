40bbf8734ca376713d487b84613443e673d38319
DhaniKWP <dhanikusuma256@gmail.com>
2025-09-27 12:29:11 +0700

    Balance, InHouse, Saving, Member

--- Models/Balance.cs
+++ Models/Balance.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Corvus.Models
 {
-    internal class Balance
+    public class Balance
     {
+        public int Id { get; set; }
+        public Member member { get; set; }
+        public decimal amount { get; set; }
+        public DateTime updateOn { get; set; }
+        public string transactionName    { get; set; }
+        public string flow { get; set; }
+
     }
 }
