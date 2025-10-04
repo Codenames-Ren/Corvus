909b3ec259d470488903bffa351e9d7b8fecc0e8
DhaniKWP <dhanikusuma256@gmail.com>
2025-10-04 11:42:39 +0700

    AdminForm, ForgotPassword

---
+++ Forms/AdminForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Corvus.Forms
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            memberToolStripMenuItem = new ToolStripMenuItem();
+            accessToolStripMenuItem = new ToolStripMenuItem();
+            productToolStripMenuItem = new ToolStripMenuItem();
+            approvalToolStripMenuItem = new ToolStripMenuItem();
+            acrossToolStripMenuItem = new ToolStripMenuItem();
+            configurationToolStripMenuItem = new ToolStripMenuItem();
+            logoutToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, memberToolStripMenuItem, accessToolStripMenuItem, productToolStripMenuItem, approvalToolStripMenuItem, acrossToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { configurationToolStripMenuItem, logoutToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "FIle";
+            fileToolStripMenuItem.Click += fileToolStripMenuItem_Click;
+            // 
+            // memberToolStripMenuItem
+            // 
+            memberToolStripMenuItem.Name = "memberToolStripMenuItem";
+            memberToolStripMenuItem.Size = new Size(64, 20);
+            memberToolStripMenuItem.Text = "Member";
+            // 
+            // accessToolStripMenuItem
+            // 
+            accessToolStripMenuItem.Name = "accessToolStripMenuItem";
+            accessToolStripMenuItem.Size = new Size(55, 20);
+            accessToolStripMenuItem.Text = "Access";
+            // 
+            // productToolStripMenuItem
+            // 
+            productToolStripMenuItem.Name = "productToolStripMenuItem";
+            productToolStripMenuItem.Size = new Size(61, 20);
+            productToolStripMenuItem.Text = "Product";
+            // 
+            // approvalToolStripMenuItem
+            // 
+            approvalToolStripMenuItem.Name = "approvalToolStripMenuItem";
+            approvalToolStripMenuItem.Size = new Size(67, 20);
+            approvalToolStripMenuItem.Text = "Approval";
+            // 
+            // acrossToolStripMenuItem
+            // 
+            acrossToolStripMenuItem.Name = "acrossToolStripMenuItem";
+            acrossToolStripMenuItem.Size = new Size(123, 20);
+            acrossToolStripMenuItem.Text = "Across Cooperation";
+            // 
+            // configurationToolStripMenuItem
+            // 
+            configurationToolStripMenuItem.Name = "configurationToolStripMenuItem";
+            configurationToolStripMenuItem.Size = new Size(180, 22);
+            configurationToolStripMenuItem.Text = "Configuration";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            logoutToolStripMenuItem.Size = new Size(180, 22);
+            logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // AdminForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "AdminForm";
+            Text = "Corvus Cooperation";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem configurationToolStripMenuItem;
+        private ToolStripMenuItem memberToolStripMenuItem;
+        private ToolStripMenuItem accessToolStripMenuItem;
+        private ToolStripMenuItem productToolStripMenuItem;
+        private ToolStripMenuItem approvalToolStripMenuItem;
+        private ToolStripMenuItem acrossToolStripMenuItem;
+        private ToolStripMenuItem logoutToolStripMenuItem;
+    }
+}
---
+++ Forms/ForgotPassword.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Corvus.Forms
+{
+    partial class ForgotPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSubmit = new Button();
+            btnBack = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtUsername = new TextBox();
+            txtColor = new TextBox();
+            txtSinger = new TextBox();
+            txtPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(36, 149);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(76, 23);
+            btnSubmit.TabIndex = 9;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(138, 149);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(76, 23);
+            btnBack.TabIndex = 10;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            btnBack.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 11;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(338, 198);
+            label2.Name = "label2";
+            label2.Size = new Size(202, 15);
+            label2.TabIndex = 12;
+            label2.Text = "What is Your Favourite Artist/Singer ?";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(36, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(165, 15);
+            label3.TabIndex = 13;
+            label3.Text = "What is Your Favourite Color ?";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(36, 277);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 14;
+            label4.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(36, 106);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(263, 23);
+            txtUsername.TabIndex = 15;
+            // 
+            // txtColor
+            // 
+            txtColor.Location = new Point(36, 230);
+            txtColor.Name = "txtColor";
+            txtColor.Size = new Size(263, 23);
+            txtColor.TabIndex = 16;
+            // 
+            // txtSinger
+            // 
+            txtSinger.Location = new Point(338, 230);
+            txtSinger.Name = "txtSinger";
+            txtSinger.Size = new Size(263, 23);
+            txtSinger.TabIndex = 17;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(36, 306);
+            txtPassword.Name = "txtPassword";
+            txtPassword.ReadOnly = true;
+            txtPassword.Size = new Size(434, 23);
+            txtPassword.TabIndex = 18;
+            // 
+            // ForgotPassword
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(668, 397);
+            Controls.Add(txtPassword);
+            Controls.Add(txtSinger);
+            Controls.Add(txtColor);
+            Controls.Add(txtUsername);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnBack);
+            Controls.Add(btnSubmit);
+            Name = "ForgotPassword";
+            Text = "Forgot Password";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSubmit;
+        private Button btnBack;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtUsername;
+        private TextBox txtColor;
+        private TextBox txtSinger;
+        private TextBox txtPassword;
+    }
+}
