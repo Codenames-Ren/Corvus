8ee67d4ea6c476b00b6995a8f76427bec391cde9
rayhan-18 <mr0031689@gmail.com>
2025-10-18 12:15:46 +0700

    Update Parahhh

---
+++ Services/MemberService.cs
@@ -0,0 +1,52 @@
+﻿using Corvus.Data;
+using Corvus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corvus.Services
+{
+    public class MemberService
+    {
+        private readonly AppDbContext _db;
+
+        public MemberService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<object> SetDropdown()
+        {
+            var list = _db.Members
+                .OrderBy(m => m.FullName)
+                .Select(m => new
+                {
+                    m.Id,
+                    DisplayName = m.MemberId + " - " + m.FullName
+                })
+                .ToList<object>();
+
+            return list;
+        }
+
+        public Member? FindById(int id)
+        {
+            return _db.Members.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Member> SetGrid()
+        {
+            return _db.Members
+                .OrderByDescending(m => m.ModDate)
+                .ToList<Member>();
+        }
+
+        public async Task Update(Member member)
+        {
+            _db.Members.Update(member);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
