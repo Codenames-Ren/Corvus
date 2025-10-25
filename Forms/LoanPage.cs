03adc041453377349ab6b6eea96bfb3b54659b1b
rayhan-18 <mr0031689@gmail.com>
2025-10-25 13:17:01 +0700

    update

--- Forms/LoanPage.cs
+++ Forms/LoanPage.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
-using BraveHeroCooperation.Utils;
+using Corvus.Utils;
 using Corvus.Data;
 using Corvus.Models;
 using Corvus.Services;
--- Util/FileHelper.cs
+++ Util/FileHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BraveHeroCooperation.Utils
+namespace Corvus.Utils
 {
     public static class FileHelper
     {
